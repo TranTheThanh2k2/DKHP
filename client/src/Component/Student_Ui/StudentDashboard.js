c0ce8474ffde571e2d5e09e9c41d26042541dfc1
Caomy0 <mynho1132002@gmail.com>
2024-05-17 22:34:32 +0700

    updatformdanggkyhp

--- client/src/Component/Student_Ui/StudentDashboard.js
+++ client/src/Component/Student_Ui/StudentDashboard.js
@@ -1,23 +1,25 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useSnackbar } from 'notistack';
-
+import { useSnackbar } from "notistack";
+import "./style_student/Studentboard.css";
+import iuh1 from "../Student_Ui/style_student/iu1.png";
+import toan from "../Student_Ui/style_student/h2.jpg";
 
 const StudentDashboard = () => {
-  const [studentId, setStudentId] = useState('');
-  const [courseId, setCourseId] = useState('');
-  const [semesterId, setSemesterId] = useState('');
-  const [fullName, setFullName] = useState('');
-  const [gender, setGender] = useState('');
-  const [departmentCode, setDepartmentCode] = useState('');
-  const [hiddenStudentId, setHiddenStudentId] = useState('');
+  const [studentId, setStudentId] = useState("");
+  const [courseId, setCourseId] = useState("");
+  const [semesterId, setSemesterId] = useState("");
+  const [fullName, setFullName] = useState("");
+  const [gender, setGender] = useState("");
+  const [departmentCode, setDepartmentCode] = useState("");
+  const [hiddenStudentId, setHiddenStudentId] = useState("");
   const [semesters, setSemesters] = useState([]);
   const [courses, setCourses] = useState([]);
   const [selectedCourses, setSelectedCourses] = useState([]);
   const { enqueueSnackbar } = useSnackbar();
 
   useEffect(() => {
-    const studentIdFromStorage = localStorage.getItem('studentID');
+    const studentIdFromStorage = localStorage.getItem("studentID");
     if (studentIdFromStorage) {
       setStudentId(studentIdFromStorage);
       setHiddenStudentId(studentIdFromStorage);
@@ -28,14 +30,16 @@ const StudentDashboard = () => {
 
   const fetchStudentDetails = async (studentId) => {
     try {
-      const response = await axios.get(`http://localhost:3000/api/students/${studentId}`);
+      const response = await axios.get(
+        `http://localhost:3000/api/students/${studentId}`
+      );
       setFullName(response.data.data.student.Full_Name);
       setDepartmentCode(response.data.data.student.Department_Code);
       setGender(response.data.data.student.Gender);
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   const fetchSemesters = async () => {
     try {
@@ -44,38 +48,43 @@ const StudentDashboard = () => {
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   const fetchCoursesBySemester = async (semesterId) => {
     try {
-      const response = await axios.get(`http://localhost:3000/courses/${semesterId}/courses`);
+      const response = await axios.get(
+        `http://localhost:3000/courses/${semesterId}/courses`
+      );
       setCourses(response.data);
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   const handleRegisterCourse = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:3000/api/course/register', {
-        studentId: studentId,
-        courseId: selectedCourses,
-        semesterId: semesterId
-      });
-      enqueueSnackbar(response.data.message, { variant: 'success'});
+      const response = await axios.post(
+        "http://localhost:3000/api/course/register",
+        {
+          studentId: studentId,
+          courseId: selectedCourses,
+          semesterId: semesterId,
+        }
+      );
+      enqueueSnackbar(response.data.message, { variant: "success" });
       setTimeout(() => {
         window.location.reload();
       }, 3000);
     } catch (error) {
       console.error(error);
-      enqueueSnackbar('Đăng ký thất bại.', { variant: 'error' });
+      enqueueSnackbar("Đăng ký thất bại.", { variant: "error" });
     }
   };
 
   const handleCheckboxChange = (courseId) => {
     if (selectedCourses.includes(courseId)) {
-      setSelectedCourses(selectedCourses.filter(id => id !== courseId));
+      setSelectedCourses(selectedCourses.filter((id) => id !== courseId));
     } else {
       setSelectedCourses([...selectedCourses, courseId]);
     }
@@ -83,32 +92,71 @@ const StudentDashboard = () => {
 
   return (
     <div>
-      <h2>Xin Chào !</h2>
-      <h1>{fullName}</h1>
-      <h3>Giới Tính : {gender}</h3>
-      <h3>Khoa : {departmentCode} </h3>
-      <form onSubmit={handleRegisterCourse}>
-        <input type="hidden" value={hiddenStudentId} />
-        <label>
-          Đợt Đăng Kí
-          <select
-            value={semesterId}
-            onChange={(e) => {
-              setSemesterId(e.target.value);
-              fetchCoursesBySemester(e.target.value);
+      <div>
+        <img
+          src={iuh1}
+          style={{
+            height: "auto",
+            width: "80%",
+            paddingLeft: 150,
+          }}
+        />
+      </div>
+      <div className="sv">
+        <div className="sv1">
+          <h3>Xin Chào !</h3>
+          <p>{fullName}</p>
+          <p>Giới Tính : {gender}</p>
+          <p>Khoa : {departmentCode} </p>
+        </div>
+        <div className="sv2">
+          <img
+            src={toan}
+            style={{
+              height: 150,
+              width: 150,
             }}
-          >
-            <option value="">-- Chọn học kỳ --</option>
-            {semesters.map((semester) => (
-              <option key={semester._id} value={semester._id}>
-                {semester.Semester_Name}
-              </option>
-            ))}
-          </select>
-        </label>
-        <button type="submit">Đăng Kí</button>
-      </form>
-      <h3>Danh sách môn học</h3>
+          />
+        </div>
+        <div className="sv3">
+          <p>THÔNG TIN SINH VIÊN</p>
+          <p>ĐĂNG KÝ HỌC PHẦN</p>
+          <p>CHƯƠNG TRÌNH KHUNG</p>
+        </div>
+      </div>
+      <div className="sv4">
+        <h2>ĐĂNG KÝ HỌC PHẦN</h2>
+      </div>
+      <div className="sv5">
+        <form onSubmit={handleRegisterCourse}>
+          <input type="hidden" value={hiddenStudentId} />
+          <label style={{ fontSize: 20, fontWeight: "bold" }}>
+            Đợt Đăng Kí
+            <select
+              style={{ marginLeft: 20, fontSize: 20 }}
+              value={semesterId}
+              onChange={(e) => {
+                setSemesterId(e.target.value);
+                fetchCoursesBySemester(e.target.value);
+              }}
+            >
+              <option value="">-- Chọn học kỳ --</option>
+              {semesters.map((semester) => (
+                <option key={semester._id} value={semester._id}>
+                  {semester.Semester_Name}
+                </option>
+              ))}
+            </select>
+          </label>
+          <button type="submit" style={{ marginLeft: 20, fontSize: 20 }}>
+            Đăng Kí
+          </button>
+        </form>
+      </div>
+      <div className="sv6">
+        <h2>Môn Học Phần Đang Chờ Đăng Ký</h2>
+      </div>
+
       <table>
         <thead>
           <tr>
@@ -136,7 +184,7 @@ const StudentDashboard = () => {
         </tbody>
       </table>
     </div>
-  );  
+  );
 };
 
 export default StudentDashboard;
